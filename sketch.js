9f09ebb48f65a53515c06095198943c9a5344daf
Priangshu2008 <78959073+Priangshu2008@users.noreply.github.com>
2021-08-20 17:51:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -52,36 +52,15 @@ async function getBackgroundImg(){
 
 
     // add conditions to change the background images from sunrise to sunset
-    if(hour>=04&&hour<=86){
-       bg="sunrise1.png"
-    }else if(hour>=06&&hour<=08){
-       bg="sunrise2.png"
-    }else if(hour>=08&&hour<=10){
-       bg="sunrise3.png"
-    }else if(hour>=10&&hour<=12){
-       bg="sunrise4.png"
-    }else if(hour>=12&&hour<=14){
-       bg="sunrise5.png"
-    }else if(hour>=14&&hour<=16){
-       bg="sunrise6.png"
-    }else if(hour>=16&&hour<=18){
-       bg="sunrise7.png"
-    }else if(hour>=18&&hour<=20){
-       bg="sunrise8.png"
-    }else if(hour>=20&&hour<=22){
-       bg="sunrise9.png"
-    }else if(hour>=22&&hour<=24){
-       bg="sunrise10.png"
-    }else if(hour>=24&&hour<=26){
-       bg="sunrise12.png"                  
-    }
-
-
-
+    if(hour>=06 && hour<=17){
+       bg="sunrise.png"
+      }else {bg="sunset.png"
+      }
+      
     //load the image in backgroundImg variable here
 
     backgroundImg = loadImage(bg);
-    console.log("backgroundImage",backgroundImg);
+   // console.log("backgroundImage",backgroundImg);
 
     
 }    
